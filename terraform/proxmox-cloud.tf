389edba724856306330f9c16f513a13b4bd10c12
UsunSzkorbut <wojtek.hajdul.2000@gmail.com>
2024-12-08 13:34:51 +0100

    Change to Ubuntu24 and IP CIDR

--- terraform/proxmox-cloud.tf
+++ terraform/proxmox-cloud.tf
@@ -1,10 +1,10 @@
 resource "proxmox_vm_qemu" "kubeadm-master" {
     target_node = "proxn1"
-    desc = "Cloud Ubuntu 22.04"
+    desc = "Cloud Ubuntu 24.04"
     count = 1
     onboot = true
 
-    clone = "ubuntu-cloud-2204"
+    clone = "ubuntu-cloud-2404"
     agent = 0
 
     os_type = "cloud-init"
@@ -40,7 +40,7 @@ resource "proxmox_vm_qemu" "kubeadm-master" {
         model = "virtio"
         bridge = "vmbr0"
     }
-    ipconfig0 = "ip=10.0.0.1${count.index + 1}/24,gw=10.0.0.1"
+    ipconfig0 = "ip=10.75.168.1${count.index + 1}/24,gw=10.75.168.1"
 }
 
 resource "proxmox_vm_qemu" "kubeadm-worker" {
@@ -85,5 +85,5 @@ resource "proxmox_vm_qemu" "kubeadm-worker" {
         model = "virtio"
         bridge = "vmbr0"
     }
-    ipconfig0 = "ip=10.0.0.2${count.index + 1}/24,gw=10.0.0.1"
+    ipconfig0 = "ip=10.75.168.2${count.index + 1}/24,gw=10.75.168.1"
 }
