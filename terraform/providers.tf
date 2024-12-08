cabd4cc039de0b26dc1130f89c216b045065115e
UsunSzkorbut <wojtek.hajdul.2000@gmail.com>
2024-12-08 13:32:59 +0100

    change IP for proxmox

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,7 +10,7 @@ terraform {
 
   provider "proxmox" {
       pm_tls_insecure = true
-      pm_api_url = "https://10.0.0.111:8006/api2/json"
+      pm_api_url = "https://10.75.168.99:8006/api2/json"
       pm_api_token_secret = "ef058ed0-f6fd-4c37-94c7-a29fdd32d389"
       pm_api_token_id = "root@pam!terraform"
   }
